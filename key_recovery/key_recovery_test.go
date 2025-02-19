00c9225b101f9ee03095980bb2399b0be91144b5
Aleksa Prtenjača <aleksa.prtenjaca03@gmail.com>
2025-02-19 23:23:07 +0100

    Recover functionality changed
    - Recover returns error if any two given shares use the same point
    - If more than t valid shares are provided, Recover recovers the key using the first t and the last t shares and returns error if they do not match
    - Tests added/modified

--- key_recovery/key_recovery_test.go
+++ key_recovery/key_recovery_test.go
@@ -4,31 +4,23 @@ import (
 	"fmt"
 	BN254_fr "github.com/consensys/gnark-crypto/ecc/bn254/fr"
 	SECP256K1_fr "github.com/consensys/gnark-crypto/ecc/secp256k1/fr"
-	"math/rand"
 	"testing"
-	"time"
 )
 
 func TestSplitAndRecover(t *testing.T) {
-	spendingKeyStr, viewingKeyStr, err := setupRandomKeys()
+	n := 20
+	threshold := 14
+
+	shares, spendingKeyStr, viewingKeyStr, err := setupRandomShares(n, threshold)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	n := 20
-	threshold := 14
-
 	t.Logf("Spending key: %s\n", spendingKeyStr)
 	t.Logf("Viewing key: %s\n", viewingKeyStr)
-
-	shares, err := Split(threshold, n, spendingKeyStr, viewingKeyStr)
-	if err != nil {
-		t.Fatalf("Failed to construct the shares: %s\n", err)
-	}
-
 	t.Logf("Shares: %v\n", shares)
 
-	t.Run("xi != 0 for all i", func(t *testing.T) {
+	t.Run("Split: xi != 0 for all i", func(t *testing.T) {
 		for _, share := range shares {
 			if share.Point == "0" {
 				t.Fatal("One share evaluation point is zero")
@@ -36,7 +28,7 @@ func TestSplitAndRecover(t *testing.T) {
 		}
 	})
 
-	t.Run("xi != xj for i != j", func(t *testing.T) {
+	t.Run("Split: xi != xj for i != j", func(t *testing.T) {
 		for i, share := range shares {
 			for j := i + 1; j < len(shares); j++ {
 				if share.Point == shares[j].Point {
@@ -46,7 +38,7 @@ func TestSplitAndRecover(t *testing.T) {
 		}
 	})
 
-	t.Run("given shares = t", func(t *testing.T) {
+	t.Run("Recover with given shares = t", func(t *testing.T) {
 		newSpendingKeyStr, newViewingKeyStr, err := Recover(threshold, shares[0:threshold])
 		t.Logf("Recovered spending key: %s\n", newSpendingKeyStr)
 		t.Logf("Recovered viewing key: %s\n", newViewingKeyStr)
@@ -61,7 +53,7 @@ func TestSplitAndRecover(t *testing.T) {
 		}
 	})
 
-	t.Run("given shares = t + 1 > t", func(t *testing.T) {
+	t.Run("Recover with given shares = t + 1 > t", func(t *testing.T) {
 		newSpendingKeyStr, newViewingKeyStr, err := Recover(threshold, shares[0:threshold+1])
 		t.Logf("Recovered spending key: %s\n", newSpendingKeyStr)
 		t.Logf("Recovered viewing key: %s\n", newViewingKeyStr)
@@ -76,44 +68,124 @@ func TestSplitAndRecover(t *testing.T) {
 		}
 	})
 
-	t.Run("given shares = t - 1 < t", func(t *testing.T) {
+	t.Run("Recover with given shares = t - 1 < t", func(t *testing.T) {
 		_, _, err := Recover(threshold, shares[0:threshold-1])
 		if err == nil {
 			t.Error("Keys should not have been reconstructed")
 		}
 	})
 
-	t.Run("given shares = t but has duplicate points", func(t *testing.T) {
+	t.Run("Recover with shares containing duplicate points", func(t *testing.T) {
 		sharesCopy := make([]Share, threshold)
 		copy(sharesCopy, shares[0:threshold])
 		sharesCopy[0].Point = sharesCopy[1].Point
+
+		t.Logf("Shares given: %v\n", sharesCopy)
+
 		_, _, err := Recover(threshold, sharesCopy[0:threshold])
 		if err == nil {
 			t.Error("Keys should not have been reconstructed")
 		}
 	})
 
-	t.Run("given shares > t and non duplicate points >= t", func(t *testing.T) {
-		sharesCopy := make([]Share, threshold*2)
-		copy(sharesCopy, shares)
-		copy(sharesCopy[threshold:], shares)
+	t.Run("Recover with given shares = t + 1 and one is modified", func(t *testing.T) {
+		sharesCopy := make([]Share, threshold+1)
+		copy(sharesCopy, shares[0:threshold+1])
 
-		r := rand.New(rand.NewSource(time.Now().UnixNano()))
-		r.Shuffle(len(sharesCopy), func(i, j int) { sharesCopy[i], sharesCopy[j] = sharesCopy[j], sharesCopy[i] })
-
-		newSpendingKeyStr, newViewingKeyStr, err := Recover(threshold, sharesCopy)
-		t.Logf("Recovered spending key: %s\n", newSpendingKeyStr)
-		t.Logf("Recovered viewing key: %s\n", newViewingKeyStr)
+		var skEval SECP256K1_fr.Element
+		_, err := skEval.SetString("0x" + sharesCopy[0].SpendingEval)
 		if err != nil {
-			t.Fatalf("Failed to recover the shares: %s\n", err)
+			t.Fatal(err)
 		}
-		if newSpendingKeyStr != spendingKeyStr {
-			t.Error("Spending keys do not match")
+		var vkEval BN254_fr.Element
+		_, err = vkEval.SetString("0x" + sharesCopy[0].ViewingEval)
+		if err != nil {
+			t.Fatal(err)
 		}
-		if newViewingKeyStr != viewingKeyStr {
-			t.Error("Viewing keys do not match")
+
+		skEval.Double(&skEval)
+		vkEval.Double(&vkEval)
+
+		sharesCopy[0].SpendingEval = skEval.Text(16)
+		sharesCopy[0].ViewingEval = vkEval.Text(16)
+
+		t.Logf("Shares given: %v\n", sharesCopy)
+
+		_, _, err = Recover(threshold, sharesCopy)
+
+		if err == nil {
+			t.Error("Keys should not have been reconstructed")
 		}
 	})
+
+}
+
+func TestExtractPointsFromShares(t *testing.T) {
+	n := 20
+	threshold := 14
+
+	shares, _, _, err := setupRandomShares(n, threshold)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	points, err := extractPointsFromShares(shares, threshold)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(points) > threshold {
+		t.Fatal("extractPointsFromShare returned more points than expected")
+	}
+}
+
+func TestPointsUnique(t *testing.T) {
+	shares, _, _, err := setupRandomShares(20, 14)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	shares = append(shares, shares[0])
+
+	valid, _ := pointsUnique(shares)
+
+	if valid {
+		t.Error("Duplicate point not detected")
+	}
+}
+
+func TestPointFromShare(t *testing.T) {
+	var sk SECP256K1_fr.Element
+	_, err := sk.SetRandom()
+	if err != nil {
+		t.Fatal(err)
+	}
+	var vk BN254_fr.Element
+	_, err = vk.SetRandom()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	s := Share{
+		Point:        "1",
+		SpendingEval: sk.Text(16),
+		ViewingEval:  vk.Text(16),
+	}
+
+	var p point
+	err = p.fromShare(s)
+	if err != nil {
+		t.Fatalf("failed to recover point from share: %s\n", err)
+	}
+
+	if !sk.Equal(&p.yS) {
+		t.Fatal("spending key does not match")
+	}
+
+	if !vk.Equal(&p.yV) {
+		t.Fatal("viewing key does not match")
+	}
+
 }
 
 func BenchmarkSplitAndRecover(b *testing.B) {
@@ -145,7 +217,7 @@ func BenchmarkSplitAndRecover(b *testing.B) {
 	})
 
 	b.Run("Recover (from chosen points)", func(b *testing.B) {
-		points, err := choosePointsFromShares(threshold, shares)
+		points, err := extractPointsFromShares(shares, threshold)
 		if err != nil {
 			b.Fatal(err)
 		}
@@ -175,4 +247,18 @@ func setupRandomKeys() (string, string, error) {
 	viewingKeyStr := viewingKey.Text(16)
 
 	return spendingKeyStr, viewingKeyStr, nil
+}
+
+func setupRandomShares(n, threshold int) ([]Share, string, string, error) {
+	spendingKeyStr, viewingKeyStr, err := setupRandomKeys()
+	if err != nil {
+		return nil, "", "", err
+	}
+
+	shares, err := Split(threshold, n, spendingKeyStr, viewingKeyStr)
+	if err != nil {
+		return nil, "", "", fmt.Errorf("Failed to construct the shares: %s\n", err)
+	}
+
+	return shares, spendingKeyStr, viewingKeyStr, nil
 }
